bead495f3c16b7dc08ad2969df85dc526b5fa846
anonymousrecords <a.Records2022@gmail.com>
2024-10-28 19:43:14 +0900

    chore: add constants

---
+++ constants/constants.ts
@@ -0,0 +1,20 @@
+export const HeaderList = [
+  {
+    title: "Docs",
+    href: `/docs/getting-started/introduction`,
+  },
+  {
+    title: "Showcase",
+    href: "/showcase",
+  },
+  {
+    title: "Blog",
+    href: "https://nextjs-blog-template.pages.dev/",
+  },
+  {
+    title: "Github",
+    href: "https://github.com/notionpresso",
+  },
+] as const;
+
+export const LanguageList = ["English", "한국어"] as const;
