0161019985d44c2cc1496d90b454cff633aa70ca
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-10-29 08:31:36 +0900

    refactor components folder tree in docs

---
+++ components/docs/index.ts
@@ -0,0 +1,7 @@
+export { default as Sidebar } from "./sidebar";
+export { default as ThemeToggle } from "./theme-toggle";
+export { default as TOC } from "./toc";
+export { default as DynamicLayout } from "./dynamic-layout";
+export { default as MobileSidebar } from "./mobile-sidebar";
+export { default as NavigationButton } from "./navigation-button";
+export { default as GuidePage } from "./guide-page";
