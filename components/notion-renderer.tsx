4d7bdeddd90766b9f919982ddf9bce2d2b73a38f
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-10-30 13:05:17 +0900

    good

---
+++ components/notion-renderer.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { Notion } from "@notionpresso/react";
+
+const NotionRenderer = ({
+  blocks,
+  title,
+  cover,
+}: {
+  blocks: any[];
+  title: string;
+  cover?: string;
+}) => {
+  return (
+    <Notion>
+      <Notion.Cover src={cover} />
+      <Notion.Body>
+        <Notion.Title title={title} />
+        <Notion.Blocks blocks={blocks} />
+      </Notion.Body>
+    </Notion>
+  );
+};
+
+export default NotionRenderer;
