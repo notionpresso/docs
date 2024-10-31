822647a169fa8081f52bbffe42128f68cd01535c
머승 <64200419+Moon-DaeSeung@users.noreply.github.com>
2024-10-31 22:19:29 +0900

    add image (#20)

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,14 +15,32 @@ export const metadata: Metadata = {
   description:
     "Comprehensive documentation for Notionpresso library, a powerful tool for rendering Notion pages in React applications.",
   keywords: "react, notion, custom, documentation, library, rendering",
+  icons: {
+    icon: "/notionpresso.jpeg",
+    shortcut: "/notionpresso.jpeg",
+    apple: "/notionpresso.jpeg",
+  },
   openGraph: {
     title: "Notionpresso Documentation",
     description:
       "Learn how to use Notionpresso to render Notion pages in your React apps.",
     type: "website",
-    // TODO: Add URL and image
-    // url: "",
-    // image: "",
+    images: [
+      {
+        url: "/notionpresso.jpeg",
+        width: 400,
+        height: 400,
+        alt: "Notionpresso Documentation",
+      },
+    ],
+    siteName: "Notionpresso",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Notionpresso Documentation",
+    description:
+      "Learn how to use Notionpresso to render Notion pages in your React apps.",
+    images: ["/notionpresso.jpeg"],
   },
 };
 
