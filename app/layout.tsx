c1fe8323bffdaa715dc8659d1f7c70bc39af1d22
anonymousrecords <a.Records2022@gmail.com>
2024-11-01 01:23:11 +0900

    chore: fix build error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-import Navigation from "@/components/navigation";
 import Header from "@/components/ui/header";
 
 const pretendard = localFont({
