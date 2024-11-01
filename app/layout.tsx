c75232d27afbd346f385339a503a76f591abc814
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-11-01 09:02:30 +0900

    fix: meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,36 +11,47 @@ const pretendard = localFont({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://notionpresso.com"),
   title: "Notionpresso Docs",
   description:
     "Comprehensive documentation for Notionpresso library, a powerful tool for rendering Notion pages in React applications.",
   keywords: "react, notion, custom, documentation, library, rendering",
   icons: {
-    icon: "/notionpresso.jpeg",
-    shortcut: "/notionpresso.jpeg",
-    apple: "/notionpresso.jpeg",
+    icon: "/icon.jpeg",
+    shortcut: "/icon.jpeg",
+    apple: "/icon.jpeg",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
   },
   openGraph: {
     title: "Notionpresso Documentation",
     description:
       "Learn how to use Notionpresso to render Notion pages in your React apps.",
     type: "website",
+    url: "https://notionpresso.com",
+    siteName: "Notionpresso",
     images: [
       {
-        url: "/notionpresso.jpeg",
-        width: 400,
-        height: 400,
+        url: "/icon.jpeg",
         alt: "Notionpresso Documentation",
       },
     ],
-    siteName: "Notionpresso",
   },
   twitter: {
     card: "summary_large_image",
     title: "Notionpresso Documentation",
     description:
       "Learn how to use Notionpresso to render Notion pages in your React apps.",
-    images: ["/notionpresso.jpeg"],
+    images: ["/icon.jpeg"],
   },
 };
 
