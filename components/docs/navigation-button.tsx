4c3cb43e27d4111d759062be84abf0dc73ee47f0
Serim Min <97885933+anonymousRecords@users.noreply.github.com>
2024-11-02 09:05:23 +0900

    fix: update link path in navigation button (#33)
    * refactor: update link path in NavigationButton component
    
    * refactor: retrieve the document of the prev or next group

--- components/docs/navigation-button.tsx
+++ components/docs/navigation-button.tsx
@@ -19,7 +19,7 @@ export default function NavigationButton({
   return (
     <Link
       className="block w-full border cursor-pointer border-black p-1 sm:p-2 rounded-sm hover:bg-black hover:text-white dark:border-gray-500 dark:hover:bg-black dark:hover:text-white text-sm sm:text-base"
-      href={`/${lang}/guide/${document.group}/${document.slug}`}
+      href={`/${lang}/docs/${document.group}/${document.slug}`}
     >
       {direction === "prev" ? (
         <div className="flex items-center gap-5 h-16">
