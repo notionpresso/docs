b356a57a7a81d56f1d7df4e5faf15c88b972ebf6
Moon-DaeSeung <tmdeoans@snu.ac.kr>
2024-11-03 17:53:05 +0900

    refactor landing

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,11 +1,8 @@
 import { LandingHero } from "@/components/landing/hero";
-import { SUPPORTED_LANGUAGES } from "@/i18n/supported-languages";
 
 export const runtime = "edge";
 
-export const generateStaticParams = () => {
-  return SUPPORTED_LANGUAGES.map((lang) => ({ lang }));
-};
+export { generateStaticParams } from "@/i18n";
 
 export default function Home() {
   return <LandingHero />;
